1feb800e28555ec6b2c92014f884e21e920b0f84
koikeyudai <yuubass1007@gmail.com>
2019-09-18 01:47:39 +0000

    herokudeploy

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 15}
+  
+  VALID_EMAIL_REGEX = /[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]/
+  validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
+  
+  VALID_PASSWORD_REGEX = /\A(?=.?[a-z])(?=.?\d)[a-z\d]{8,32}\z/i
+  validates :password, presence: true, format: {with: VALID_PASSWORD_REGEX}
+  
+  has_secure_password
+  
+  has_many :topics
+  has_many :favorites
+  has_many :favorite_topics, through: :favorites, source: 'topic'
+  has_many :comments
+end
---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+  get 'topics/new'
+  get 'sessions/new'
+  
+  get 'pages/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+  get 'pages/help'
+  
+  resources :users
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
+  resources :users
+  resources :topics
+  
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+  
+  get 'comments/new', to: 'comments#new'
+  post '/comments', to: 'comments#create'
+end
